c96987608a0d00e0de4e1bfe60c4fcd339e071f6
tcpassos <tcpassos99@gmail.com>
2021-06-04 21:24:09 -0300

    Primeiro commit

---
+++ src/main/java/criticalpath/CriticalPathTest.java
@@ -0,0 +1,9 @@
+package criticalpath;
+
+public class CriticalPathTest {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/java/criticalpath/Vertex.java
@@ -0,0 +1,9 @@
+package criticalpath;
+
+public class Vertex {
+
+    public Vertex(int n) {
+        // Implementar a logica de inclusao do vertice aqui
+    }
+    
+}
